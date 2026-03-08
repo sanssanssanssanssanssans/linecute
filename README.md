3d42d9c4c6c3608d4d8bfb676ee024becdf321ce
sanssanssanssanssanssans <ajona5427@kakao.com>
2026-03-08 10:55:30 +0900

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# linecute
+
+바탕화면에서 귀여운 라인이를 보실 수 있습니다!!!!!!!
+![screenshot](JeaMakiba.png)
---
+++ main.py
@@ -0,0 +1,67 @@
+import sys, os
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QMovie
+from PySide6.QtWidgets import QApplication, QLabel
+import sys, os
+if getattr(sys, 'frozen', False):
+    BASE = sys._MEIPASS
+else:
+    BASE = os.path.dirname(__file__)
+
+GIF_PATH = os.path.join(BASE, r"1759644087.gif")
+class FloatingGif(QLabel):
+    def __init__(self, gif_path):
+        super().__init__()
+        self.setWindowFlags(
+            Qt.FramelessWindowHint |
+            Qt.WindowStaysOnTopHint |
+            Qt.Tool
+        )
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setScaledContents(True)
+
+        self.movie = QMovie(gif_path)
+        self.setMovie(self.movie)
+        self.movie.start()
+
+        self.resize(320, 320)
+        self.move(200, 200)
+
+        self.drag_pos = None
+        self.opacity = 1.0
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            self.drag_pos = e.globalPosition().toPoint() - self.frameGeometry().topLeft()
+
+    def mouseMoveEvent(self, e):
+        if self.drag_pos and e.buttons() & Qt.LeftButton:
+            self.move(e.globalPosition().toPoint() - self.drag_pos)
+
+    def mouseReleaseEvent(self, e):
+        self.drag_pos = None
+
+    def wheelEvent(self, e):
+        if QApplication.keyboardModifiers() & Qt.ControlModifier:
+            delta = 0.05 if e.angleDelta().y() > 0 else -0.05
+            self.opacity = max(0.3, min(1.0, self.opacity + delta))
+            self.setWindowOpacity(self.opacity)
+        else:
+            step = 30 if e.angleDelta().y() > 0 else -30
+            w = max(80, self.width() + step)
+            h = max(80, self.height() + step)
+            self.resize(w, h)
+
+    def contextMenuEvent(self, e):
+        from PySide6.QtWidgets import QMenu, QFileDialog
+        menu = QMenu(self)
+        act_exit = menu.addAction("종료")
+        act = menu.exec(e.globalPos())
+        if act == act_exit:
+            QApplication.quit()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = FloatingGif(GIF_PATH)
+    w.show()
+    sys.exit(app.exec())
